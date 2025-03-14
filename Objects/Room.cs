1ce1e38b5b6316c0e46b6762adbf5bada9399b6f
gjstoub <gavin.stoub@cuw.edu>
2025-03-14 10:36:48 -0500

    Update Room.cs

--- Objects/Room.cs
+++ Objects/Room.cs
@@ -1,99 +1,49 @@
-using System;
-using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Room
+public class Room : MonoBehaviour
 {
+    private static List<string> visitedRooms = new List<string>();
+    private static Dictionary<string, Vector2> roomPositions = new Dictionary<string, Vector2>();
+    private static Transform miniMapParent;
+    private static GameObject roomPrefab;
+
     private Player thePlayer;
-
-    private GameObject[] theDoors;
-    private Exit[] availableExits = new Exit[4];
-    private int currNumberOfExits = 0;
-
     private string name;
-
+    
     public Room(string name)
     {
         this.name = name;
-        this.thePlayer = null;
-    }
 
-    public string getName()
-    {
-        return this.name;
-    }
-
-    public bool tryToTakeExit(string direction)
-    {
-        Exit theExit = this.getExit(direction);
-        if(theExit != null)
+        if (miniMapParent == null)
         {
-            //remove the player from the current room
-            Core.thePlayer.getCurrentRoom().removePlayer();
-
-            //place them in the destination room in that direction
-            Room destinationRoom = theExit.getDestination();
-            destinationRoom.setPlayer(Core.thePlayer);
-            
-            //update the room the player is currently in so the room exits visually update
-            return true;
+            miniMapParent = GameObject.Find("MiniMap").transform;
+            roomPrefab = Resources.Load<GameObject>("MiniMapRoom");
         }
-        else
-        {
-            Debug.Log("No Exit In This Direction");
-            return false;
-        }
-    }
-
-    private Exit getExit(string direction)
-    {
-        if(this.hasExit(direction))
-        {
-            for(int i = 0; i < this.currNumberOfExits; i++)
-            {
-                if(String.Equals(this.availableExits[i].getDirection(), direction))
-                {
-                    return this.availableExits[i];
-                }
-            }
-        }
-        return null;
-    }
-
-    public bool hasExit(string direction)
-    {
-        for(int i = 0; i < this.currNumberOfExits; i++)
-        {
-            if(String.Equals(this.availableExits[i].getDirection(), direction))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void removePlayer()
-    {
-        this.thePlayer = null;
     }
 
     public void setPlayer(Player p)
     {
         this.thePlayer = p;
         this.thePlayer.setCurrentRoom(this);
+
+        if (!visitedRooms.Contains(name))
+        {
+            visitedRooms.Add(name);
+            Vector2 newPosition = GetNewRoomPosition();
+            roomPositions[name] = newPosition;
+
+            GameObject roomIcon = GameObject.Instantiate(roomPrefab, miniMapParent);
+            roomIcon.GetComponent<RectTransform>().anchoredPosition = newPosition;
+        }
     }
-    public void addExit(string direction, Room destination)
+
+    private Vector2 GetNewRoomPosition()
     {
-        if(this.currNumberOfExits <= 3)
-        {
-            Exit e = new Exit(direction, destination);
-            this.availableExits[this.currNumberOfExits] = e;
-            this.currNumberOfExits++;
-        }
-        else
-        {
-            Console.Error.WriteLine("there are too many exits!!!!");
-        }
+        if (visitedRooms.Count == 1) return Vector2.zero;
+        return new Vector2(visitedRooms.Count * 50, 0);
     }
+}
 
 }
